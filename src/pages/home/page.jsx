c9539d229079c21dc9ff812f6b494eec6b642254
SampreetG <sampreet.gumparla@nextinvo.com>
2024-02-17 17:00:05 +0530

    Added loader

--- src/pages/home/page.jsx
+++ src/pages/home/page.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { CircularProgress } from "@mui/material";
 import "./page.css";
 export default function Home() {
   const [profileData, setProfileData] = useState({
@@ -9,8 +10,10 @@ export default function Home() {
     gender: "",
     phone: "",
   });
+  const [loading, setLoading] = useState(false);
 
   const getUserDetails = () => {
+    setLoading(true);
     axios({
       method: "GET",
       url: "https://randomuser.me/api/?page=1&results=1&seed=abc",
@@ -25,9 +28,11 @@ export default function Home() {
           gender: data.gender,
           phone: data.phone,
         });
+        setLoading(false);
       })
       .catch((err) => {
         console.log(err);
+        setLoading(false);
       });
   };
 
@@ -35,24 +40,37 @@ export default function Home() {
   return (
     <div className="user-card-outer">
       <div className="user-card-inner">
-        <div className="imageCard">
-          <img src={profileData.image} alt="userImage" className="userImage" />
-        </div>
-        <div className="user-details-container">
-          <div className="name">
-            <p className="textElements person">{profileData.title}&nbsp;</p>
-            <p className="textElements person">{profileData.fName}&nbsp;</p>
-            <p className="textElements person">{profileData.lName}</p>
+        {loading ? (
+          <div className="loaderSection">
+            <CircularProgress thickness={3} size={70} className="loader" />
+            <p>Getting profile...</p>
           </div>
-          <p className="textElements">
-            <span className="sectionTitle">Gender: </span>
-            {profileData.gender}
-          </p>
-          <p className="textElements">
-            <span className="sectionTitle">Phone: </span>
-            {profileData.phone}
-          </p>
-        </div>
+        ) : (
+          <>
+            <div className="imageCard">
+              <img
+                src={profileData.image}
+                alt="userImage"
+                className="userImage"
+              />
+            </div>
+            <div className="user-details-container">
+              <div className="name">
+                <p className="textElements person">{profileData.title}&nbsp;</p>
+                <p className="textElements person">{profileData.fName}&nbsp;</p>
+                <p className="textElements person">{profileData.lName}</p>
+              </div>
+              <p className="textElements">
+                <span className="sectionTitle">Gender: </span>
+                {profileData.gender}
+              </p>
+              <p className="textElements">
+                <span className="sectionTitle">Phone: </span>
+                {profileData.phone}
+              </p>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
